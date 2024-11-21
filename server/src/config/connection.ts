a22b13e55c3a55c5588e15f1d4b4ad813e94def7
raze10339 <raze10339@yahoo.com>
2024-11-21 13:05:59 -0500

    fixed connection

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -1,6 +1,6 @@
 
 import mongoose from 'mongoose';
 
-mongoose.connect('mongodb://localhost:27017/pet_social_network_db');
+mongoose.connect(process.env.MONGODB_URL ||'mongodb://localhost:27017/pet_social_network_db');
 
 export default mongoose.connection;
